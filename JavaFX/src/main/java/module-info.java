26b7581f73a06fb484a9847af3ba4af4a14aeb72
Balataa <153867553+Balataa@users.noreply.github.com>
2025-05-04 17:06:53 +0300

    hello

--- JavaFX/src/main/java/module-info.java
+++ JavaFX/src/main/java/module-info.java
@@ -10,7 +10,7 @@ module example.com.example {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+    //requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
     // Spring for WebSocket
